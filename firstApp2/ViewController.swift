de64f46634c1d1cc82a89d98bc3a3e49bd4ce295
Mark Wilson <markwilson@marks-macbook-air.local>
2018-09-17 21:39:43 -0400

    firstApp v1

--- firstApp2/ViewController.swift
+++ firstApp2/ViewController.swift
@@ -10,9 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var buttonCount = 0
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
+    @IBAction func buttonTap(_ sender: Any) {
+        
+        buttonCount += 1
+        print(buttonCount)
+        
+        view.backgroundColor = UIColor.red
+        myLabel.text = "Church Missional Moleskin"
+        
+        if buttonCount >= 10 {
+            view.backgroundColor = UIColor.blue
+            myLabel.text = "My Church Mission Book"
+        }
+        if buttonCount >= 15 {
+            view.backgroundColor = UIColor.green
+            myLabel.text = "Pass book onto New Pastor!"
+        }
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+       
+        
     }
 
     override func didReceiveMemoryWarning() {
